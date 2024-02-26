81987ba44c235735b695b0090f209987d9dc8767
Sarrnot <urassus@gmail.com>
2024-02-26 19:03:32 +0100

    fix database init

--- server/prisma/migrations/20240226180220_init/migration.sql
+++ server/prisma/migrations/20240226180220_init/migration.sql
@@ -7,11 +7,11 @@ CREATE TABLE "LedState" (
 );
 
 -- CreateTable
-CREATE TABLE "LED" (
+CREATE TABLE "Led" (
     "id" SERIAL NOT NULL,
     "ledStateId" INTEGER NOT NULL,
 
-    CONSTRAINT "LED_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Led_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -28,6 +28,7 @@ CREATE TABLE "LedConfiguration" (
 CREATE TABLE "Log" (
     "id" SERIAL NOT NULL,
     "timestamp" TIMESTAMP(3) NOT NULL,
+    "ledId" INTEGER NOT NULL,
     "ledStateId" INTEGER NOT NULL,
 
     CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
@@ -37,22 +38,19 @@ CREATE TABLE "Log" (
 CREATE UNIQUE INDEX "LedState_state_key" ON "LedState"("state");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "LED_ledStateId_key" ON "LED"("ledStateId");
-
--- CreateIndex
 CREATE UNIQUE INDEX "LedConfiguration_gpio_pin_key" ON "LedConfiguration"("gpio_pin");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "LedConfiguration_ledId_key" ON "LedConfiguration"("ledId");
 
--- CreateIndex
-CREATE UNIQUE INDEX "Log_ledStateId_key" ON "Log"("ledStateId");
+-- AddForeignKey
+ALTER TABLE "Led" ADD CONSTRAINT "Led_ledStateId_fkey" FOREIGN KEY ("ledStateId") REFERENCES "LedState"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "LED" ADD CONSTRAINT "LED_ledStateId_fkey" FOREIGN KEY ("ledStateId") REFERENCES "LedState"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "LedConfiguration" ADD CONSTRAINT "LedConfiguration_ledId_fkey" FOREIGN KEY ("ledId") REFERENCES "Led"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "LedConfiguration" ADD CONSTRAINT "LedConfiguration_ledId_fkey" FOREIGN KEY ("ledId") REFERENCES "LED"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Log" ADD CONSTRAINT "Log_ledId_fkey" FOREIGN KEY ("ledId") REFERENCES "Led"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Log" ADD CONSTRAINT "Log_ledStateId_fkey" FOREIGN KEY ("ledStateId") REFERENCES "LedState"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
