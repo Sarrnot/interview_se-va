47fb74992cb10aa6f4e6e7003abe1ad7b0a3641c
Sarrnot <urassus@gmail.com>
2024-02-28 18:30:25 +0100

    implement GET /leds/:id/interval

--- server/src/ApiController/LedsApi.ts
+++ server/src/ApiController/LedsApi.ts
@@ -19,15 +19,32 @@ LedsApi.get("/:id/state", async (req, res) => {
         throw createHttpError(404, "LED not found");
     }
 
-    res.send(led.ledState.state);
+    res.status(200).send(led.ledState.state);
 });
 
 LedsApi.put("/:id/state", async (req, res) => {
     res.send("Put LED State");
 });
 
-LedsApi.get("/:id/interval", async (_, res) => {
-    res.send("Get LED Interval");
+LedsApi.get("/:id/interval", async (req, res) => {
+    const id = idSchema.parse(Number(req.params.id));
+
+    const led = await Database.led.findFirst({
+        where: { id },
+        select: { ledConfiguration: true },
+    });
+
+    if (!led) {
+        throw createHttpError(404, "LED not found");
+    }
+    if (!led.ledConfiguration) {
+        throw createHttpError(
+            500,
+            "Internal server error: LED doesn't have a configuration."
+        );
+    }
+
+    res.status(200).send(`${led.ledConfiguration.blink_rate}`);
 });
 
 LedsApi.put("/:id/interval", async (req, res) => {
